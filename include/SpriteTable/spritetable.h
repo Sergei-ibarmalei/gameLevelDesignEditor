386fcb43bfcd141bfcd96b6f1f92f17c24af557d
Sergei-ibarmalei <guardian693@gmail.com>
2025-12-23 18:33:52 +0500

    Atlas in spritetable

--- include/SpriteTable/spritetable.h
+++ include/SpriteTable/spritetable.h
@@ -2,6 +2,7 @@
 
 
 #include <SDL.h>
+#include <SDL_image.h>
 #include <vector>
 #include "logs.h"
 #include "position.h"
@@ -82,5 +83,35 @@ struct Mechanic
     float fullPath{ 0.0f };
     ChosenRect chRect;
     std::vector<Sprite> vectorSprite;
+};
+
+class Atlas
+{
+private:
+    bool init{ true };
+    std::vector<SDL_Rect> sourceRects;
+    SDL_Texture* atlasTexture{ nullptr };
+    [[nodiscard]] bool MakeAtlas(SDL_Renderer* r, size_t size,
+        const char** filePath);
+public:
+    Atlas(SDL_Renderer* r, size_t s, const char** filePath);
+    ~Atlas();
+    Atlas(const Atlas&) = delete;
+    Atlas(Atlas&&) = delete;
+    Atlas& operator=(Atlas&&) = delete;
+    Atlas& operator=(const Atlas&) = delete;
+
+    SDL_Texture* GetAtlasTexture() const { return atlasTexture; }
+    const std::vector<SDL_Rect>& GetSourceRects() const { return sourceRects; }
+    bool Status() const { return init; }
+
+};
+
+
+class SpriteTable
+{
+private:
+
+public:
 };
 
